42179bc16bbe49ebcdc03efc372d51349d0f37b1
Muhtadi Mansib <mhtdmansib5@outlook.com>
2023-12-23 05:36:43 +0600

    Add Transaction and Delivery Class

---
+++ Dropify/Delivery.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dropify
+{
+    class Delivery
+    {
+        public Guid DeliveryID { get; set; }
+        public string DeliveryType { get; set; }
+        public string PackageDimension { get; set; }
+        public int PackageWeightKg { get; set; }
+        public string PackageContent { get; set; }
+        public Guid Sender { get; set; }
+        public string Receiver { get; set; }
+        public string ReceiverName { get; set; }
+        public Guid PickupAddress { get; set; }
+        public Guid DestinationAddress { get; set; }
+        public Guid TransactionID { get; set; }
+        public string DeliveryPersonID { get; set; }
+        public string DeliveryStatus { get; set; }
+        public string ExpectedDelivery { get; set; }
+        public DateTimeOffset DaliveryCompletedOn { get; set; }
+        public Image DeliveryProof { get; set; }
+        public string DeliveryNote { get; set; }
+    }
+}
---
+++ Dropify/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dropify
+{
+    class Transaction
+    {
+        public Guid TansactionID { get; set; }
+        public string PaymentMethod { get; set; }
+        public double Amoount { get; set; }
+        public string Currency { get; set; }
+        public DateTimeOffset TransactionTime { get; set; }
+    }
+}
