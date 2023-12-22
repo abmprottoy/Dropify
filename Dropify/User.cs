384b3ef3e04c1e22c98c327eeb60a81abfdb9233
Muhtadi Mansib <mhtdmansib5@outlook.com>
2023-12-22 12:38:31 +0600

    Add User Properties

--- Dropify/User.cs
+++ Dropify/User.cs
@@ -11,5 +11,17 @@ namespace Dropify
         public Guid ProfileID { get; set; }
         public string PasswordHash { get; set; }
         public string Role { get; set; }
+        public string Email { get; set; }
+        public string  CountryCOde { get; set; }
+        public string SecurityQuestion { get; set; }
+        public string SecurityAnswer { get; set; }
+        public string  ProfileStatus { get; set; }
+        public string  FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DOB { get; set; }
+        public string  Gender { get; set; }
+        public string  GovtIDType { get; set; }
+        public string GovtID { get; set; }
+
     }
 }
