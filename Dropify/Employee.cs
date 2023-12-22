eb6bc91a2cf54e28d644fd6908d07868e85015b0
Muhtadi Mansib <mhtdmansib5@outlook.com>
2023-12-22 13:05:38 +0600

    Add Employee Properties

---
+++ Dropify/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dropify
+{
+    class Employee
+    {
+        public Guid ProfileID { get; set; }
+        public string PasswordHash { get; set; }
+        public string Role { get; set; }
+        public string Email { get; set; }
+        public string CountryCOde { get; set; }
+        public string SecurityQuestion { get; set; }
+        public string SecurityAnswer { get; set; }
+        public string ProfileStatus { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DOB { get; set; }
+        public string Gender { get; set; }
+        public string GovtIDType { get; set; }
+        public string GovtID { get; set; }
+        public Address address { get; set; }
+    }
+}
